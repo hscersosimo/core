fa83485d40626a5a26bd66fc9839ecc1d3dc372f
Hernan Cersosimo <hscersosimo@gmail.com>
2023-01-09 15:39:30 -0300

    react-bootstrap + new tooltips method (test)

--- app/docs/content/tooltips/page.js
+++ app/docs/content/tooltips/page.js
@@ -1,60 +1,45 @@
 "use client";
 import SectionHeader from "../_ui/SectionHeader";
 import Container from "../_ui/Container";
-import { useEffect } from "react";
+import React, { useState, useRef } from "react";
+import Button from "react-bootstrap/Button";
+import Overlay from "react-bootstrap/Overlay";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 const Tooltips = () => {
-  useEffect(() => {
-    // const tooltipTriggerList = document.querySelectorAll(
-    //   '[data-bs-toggle="tooltip"]'
-    // );
-    // const tooltipList = [...tooltipTriggerList].map(
-    //   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-    // );
-  });
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Simple tooltip
+    </Tooltip>
+  );
 
   return (
     <>
       <SectionHeader name="Tooltips" icon="sidebar-flip" />
       <Container>
-        <div className="container p-5">
-          <button
-            type="button"
-            className="btn btn-secondary me-2"
-            data-bs-toggle="tooltip"
-            data-bs-placement="top"
-            title="Tooltip on top"
-          >
-            Tooltip on top
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary me-2"
-            data-bs-toggle="tooltip"
-            data-bs-placement="right"
-            title="Tooltip on right"
-          >
-            Tooltip on right
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary me-2"
-            data-bs-toggle="tooltip"
-            data-bs-placement="bottom"
-            title="Tooltip on bottom"
-          >
-            Tooltip on bottom
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary me-2"
-            data-bs-toggle="tooltip"
-            data-bs-placement="left"
-            title="Tooltip on left"
-          >
-            Tooltip on left
-          </button>
-        </div>
+        <Button ref={target} onClick={() => setShow(!show)}>
+          Click me!
+        </Button>
+        <Overlay target={target.current} show={show} placement="right">
+          {(props) => (
+            <Tooltip id="overlay-example" {...props}>
+              My Tooltip
+            </Tooltip>
+          )}
+        </Overlay>
+      </Container>
+
+      <Container>
+        <OverlayTrigger
+          placement="right"
+          delay={{ show: 250, hide: 400 }}
+          overlay={renderTooltip}
+        >
+          <Button variant="success">Hover me to see</Button>
+        </OverlayTrigger>
       </Container>
     </>
   );

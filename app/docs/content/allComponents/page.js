ae822e6039a66953a8f07a3c3328aec52c1ba23e
Hernan Cersosimo <hscersosimo@gmail.com>
2023-02-10 15:13:08 -0300

    component path fix

--- app/docs/content/allComponents/page.js
+++ app/docs/content/allComponents/page.js
@@ -1,12 +1,12 @@
 import TypographyPage from "../typography/page";
 import AlertsToastsPage from "../alertsToasts/page";
-import BadgesPage from "../Badges/page";
-import CardsPage from "../Cards/page";
-import CarouselPage from "../Carousel/page";
-import ModalsPage from "../Modals/page";
-import ProgressBarsPage from "../ProgressBars/page";
-import SpinnersPage from "../Spinners/page";
-import TooltipsPopoversPage from "../TooltipsPopovers/page";
+import BadgesPage from "../badges/page";
+import CardsPage from "../cards/page";
+import CarouselPage from "../carousel/page";
+import ModalsPage from "../modals/page";
+import ProgressBarsPage from "../progressBars/page";
+import SpinnersPage from "../spinners/page";
+import TooltipsPopoversPage from "../tooltipsPopovers/page";
 
 const AllComponents = () => {
   return (
